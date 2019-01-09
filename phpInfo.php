e154a4fab19de336551d417671d9635c2ef2d327
Mike <matroxmike@icloud.com>
2019-01-09 21:59:34 +1100

    Info demo

---
+++ phpInfo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+                      "http://www.w3.org/TR/html401/loose.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" 
+      content="text/html; charset=iso-8859-1">
+  <title>ArrayTest</title>
+</head>
+<body bgcolor="#f800f8">
+  <h1>
+  <?php phpinfo(); 
+  ?>
+  </h1>
+</body>
+</html>
